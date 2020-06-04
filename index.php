5bddd2fdfd3dc8eda88ec9505e080aa5a0a938d3
Emanuele <em.zaccaria@gmail.com>
2020-06-04 16:53:42 +0200

    Prove sugli oggetti

--- index.php
+++ index.php
@@ -2,17 +2,28 @@
 
 echo "Ciao";
 
+// questa è una classe che compone l'oggetto
+
   class Prodotto {
 
-    private $id;
-    private $type;
-    private $cost;
-    private $price;
-    private $name;
+    // questi sono gli attributi che compongono una classe
 
-
+    public $id;
+    public $type;
+    public $cost;
+    public $price;
+    public $name;
 
   }
+
+  // questi sono gli oggetti che sono composti da classi che a loro volta hanno attributi
+  // oggetti diversi possono avere una classe in comune
+
+  $oggetto1 = new Prodotto(1, "Computer", 50, 99, "Macbook");
+  echo $oggetto1 -> id;
+
+  $oggetto2 = new Prodotto(2, "Casa", 3, 15, "Padella");
+  echo $oggetto2 -> id;
 
 
 
